b4695013d7583a223d894532b0139ba8e8e48d8c
Sachin Saini <iamsainisachin@gmail.com>
2019-10-27 21:07:44 +0530

    initial commit

---
+++ app/src/main/java/com/example/droiddesserts/fabactivity.java
@@ -0,0 +1,14 @@
+package com.example.droiddesserts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fabactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fabactivity);
+    }
+}
